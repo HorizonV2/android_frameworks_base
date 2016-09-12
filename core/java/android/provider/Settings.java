bc59997e474a2e602f2cfca488e6781481bd6e59
Danny Baumann <dannybaumann@web.de>
2016-09-12 11:38:58 +0200

    Allow adjusting screen density to smaller sizes.
    Accomodates for 6" 1080x1920 devices that run at 480dpi by default, but
    those physical resolution is ~360dpi.
    
    Change-Id: I82973b097612f1e7d26af9fdbb3ac2ae28c6b93b
    (cherry picked from commit 99f23aba6b0e86fabee1fa63f12c6de4643ac81b)
    (cherry picked from commit e82456fb1636a1ad0dc5142eb76b5ce2c6805969)
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6553,11 +6553,6 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
-         * @hide
-         */
-        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
-
-        /**
          * Whether to display 4G icon instead LTE
          * @hide
          */
@@ -6898,11 +6893,6 @@ public final class Settings {
          */
         public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
                 "notification_guts_kill_app_button";
-
-        /**
-         * @hide
-         */
-        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
