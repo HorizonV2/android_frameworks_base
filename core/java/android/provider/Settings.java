dcee37f31f11ee57b3daea6e76a89261b2457cf8
elpaablo <donopwhile1@gmail.com>
2022-08-14 03:41:17 +0100

    Statusbar: adjustable max visible notification icons [1/2]
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6811,6 +6811,12 @@ public final class Settings {
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
 
         /**
+         * Max visible notification icons in the Statusbar
+         * @hide
+         */
+        public static final String MAX_VISIBLE_NOTIFICATION_ICONS = "max_visible_notification_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
