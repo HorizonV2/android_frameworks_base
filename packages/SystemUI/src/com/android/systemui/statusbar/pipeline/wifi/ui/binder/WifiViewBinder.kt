a5dc0c98708a68a332b4237a752309d61a489f34
=Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-04-02 17:59:55 +0000

    SystemUI: Introduce wifi standard icon feature [1/2] * icons taken from: a799a35c5527482f2d577783170b0478b8021b3f "base: Refactor Wi-Fi standard icons"
    [neobuddy89: Added wifi 7 icon]
    
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/ui/binder/WifiViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/ui/binder/WifiViewBinder.kt
@@ -66,6 +66,7 @@ object WifiViewBinder {
         val activityContainerView = view.requireViewById<View>(R.id.inout_container)
         val airplaneSpacer = view.requireViewById<View>(R.id.wifi_airplane_spacer)
         val signalSpacer = view.requireViewById<View>(R.id.wifi_signal_spacer)
+        val wifiStandardView = view.requireViewById<ImageView>(R.id.wifi_standard)
 
         view.isVisible = true
         iconView.isVisible = true
